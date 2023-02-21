cdced7660f341c811e7b4ac4d63556442b670259
wac <wabaham9@gmail.com>
2023-02-20 21:56:58 -0800

    jQuery

---
+++ microverse/index.js
@@ -0,0 +1,7 @@
+/* 2D array */
+arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+for (let i = arr.length; i > 0; i--) {
+    for (let j = arr[i].length; j > 0; j--) {
+        console.log(arr[i][j]);
+    }
+}
